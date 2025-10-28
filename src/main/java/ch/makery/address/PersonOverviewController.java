0b63158cdcc128396710ce804180abae759eb2af
aleksander <aleksander.tapsi@voco.ee>
2025-10-28 15:18:57 +0200

    Part 1: Scene Builder

---
+++ src/main/java/ch/makery/address/PersonOverviewController.java
@@ -0,0 +1,19 @@
+package ch.makery.address;
+
+import javafx.fxml.FXML;
+
+public class PersonOverviewController {
+    private MainApp mainApp;
+
+    public PersonOverviewController() {
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
