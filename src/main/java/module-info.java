cd3b5b1c0801046dbd04927df34508ec212096cf
aleksander <aleksander.tapsi@voco.ee>
2025-11-04 12:33:38 +0200

    Part 5: Storing Data as XML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,19 @@
 module ch.makery.address {
     requires javafx.controls;
     requires javafx.fxml;
+    // piltide kasutamiseks
     requires javafx.graphics;
+    requires java.prefs;
 
+    // JAXB
+    requires jakarta.xml.bind;
 
-
-    opens ch.makery.address to javafx.fxml;
-    exports ch.makery.address;
-    exports ch.makery.address.view;
+    // Anna FXML-le peegeldusligipääs kontrolleritele
     opens ch.makery.address.view to javafx.fxml;
+
+    // Ava JAXB-le *ja* Glassfishi implementatsioonile (core + runtime)
+    opens ch.makery.address.model to jakarta.xml.bind, org.glassfish.jaxb.core, org.glassfish.jaxb.runtime;
+    opens ch.makery.address.util  to jakarta.xml.bind, org.glassfish.jaxb.core, org.glassfish.jaxb.runtime;
+
+    exports ch.makery.address;
 }
